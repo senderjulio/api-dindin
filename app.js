defa148bde96e0492208e0e3876c48de9c346ec2
Julio Sousa Sender <julio.sender@neon.com.br>
2022-04-18 20:48:26 -0300

    Base projeto

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+app.use(cors());
+app.use(routes);
+
+app.listen(4000, ()=> console.log('Servidor aberto na porta 4000'));
